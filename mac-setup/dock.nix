5cf8bf0803ba9b76a16f74ca8e764e7de54b4a5f
ryanfanchiotti <ryan@rur.com>
2025-02-20 19:38:47 -0500

    update flake, switch dock from paths to strings

--- mac-setup/dock.nix
+++ mac-setup/dock.nix
@@ -7,12 +7,12 @@
 
     # List of apps to show in the dock (plus Finder and open apps)
     persistent-apps = [
-      /System/Applications/Messages.app
-      /System/Applications/Notes.app
-      /System/Applications/Weather.app
-      /System/Applications/Calendar.app
-      /Applications/Firefox.app
-      /Applications/WezTerm.app
+      "/System/Applications/Messages.app"
+      "/System/Applications/Notes.app"
+      "/System/Applications/Weather.app"
+      "/System/Applications/Calendar.app"
+      "/Applications/Firefox.app"
+      "/Applications/WezTerm.app"
     ];
 
     # Disable hot corner actions
